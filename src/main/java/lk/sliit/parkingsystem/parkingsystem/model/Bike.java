68cf210efa6d9d06bf149c94086597b647633762
it24101026 <it24101026@my.sliit.lk>
2025-05-18 02:03:18 +0530

    adding Bike.java Car.java& Truck.java

---
+++ src/main/java/lk/sliit/parkingsystem/parkingsystem/model/Bike.java
@@ -0,0 +1,13 @@
+package lk.sliit.parkingsystem.parkingsystem.model;
+
+public class Bike extends Vehicle{
+    public Bike(String licensePlate, String ownerId) {
+        super(licensePlate, ownerId);
+        this.type = "Bike";
+    }
+
+    public Bike(String vehicleId, String licensePlate, String ownerId) {
+        super(vehicleId, licensePlate, ownerId);
+        this.type = "Bike";
+    }
+}
